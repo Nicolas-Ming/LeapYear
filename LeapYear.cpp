af0394dfd48861b236719e1b7cfd884bd354580f
Nicolas Ming <nicming@nicolasming.com>
2022-07-20 13:51:16 -0700

    Update LeapYear.cpp

--- LeapYear.cpp
+++ LeapYear.cpp
@@ -7,16 +7,11 @@ int main()
 {
     cout << "Input a year to see if it is a leap year, then hit enter: ";
     cin >> year;
-    if ((year % 4) == 0 && (year % 100) != 0) {
+    if ((year % 4) == 0 && (year % 100) != 0 || (year % 400) == 0) {
         cout << endl << "The year " << year << " is a LEAP YEAR!!!";
     }
     else {
-        if (year % 400 == 0) {
-            cout << endl << "The year " << year << " is a LEAP YEAR!!!";
-        }
-        else {
             cout << endl << "The year " << year << " is not a leap year :(";
-        }
     }
     return 0;
 }
